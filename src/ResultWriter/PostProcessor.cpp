10702c717c00e8abc95f0d24988f8519e286839c
Vishal Sontakke <vishal.sontakke@tum.de>
2016-10-28 18:35:25 +0200

    Add the integrationMask to integrateQuantities

--- src/ResultWriter/PostProcessor.cpp
+++ src/ResultWriter/PostProcessor.cpp
@@ -2,13 +2,17 @@
 #include "SeisSol.h"
 
 void seissol::writer::PostProcessor::integrateQuantities(const double i_timestep, double* io_integrals, double* i_dofs) {
-	for( int i = 0; i < 9; i++ ) {
-		io_integrals[i] += i_dofs[NUMBER_OF_ALIGNED_BASIS_FUNCTIONS*i]*i_timestep;
+	unsigned int nextId = 0;
+	for( unsigned int i = 0; i < 9; i++ ) {
+		if (m_integrationMask[i]) {
+			io_integrals[nextId] += i_dofs[NUMBER_OF_ALIGNED_BASIS_FUNCTIONS*i]*i_timestep;
+			nextId++;
+		}
 	}
 }
 
 void seissol::writer::PostProcessor::setIntegrationMask(const int * const i_integrationMask) {
-	for (size_t i = 0; i < 9; i++) {
+	for (unsigned int i = 0; i < 9; i++) {
 		m_integrationMask[i] = (bool)i_integrationMask[i];
 		if (m_integrationMask[i]) {
 			m_numberOfVariables++;
