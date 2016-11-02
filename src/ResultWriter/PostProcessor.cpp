0eb83af0bd66648e1e434a0aa6225c2809399cbf
Vishal Sontakke <vishal.sontakke@tum.de>
2016-11-02 15:29:42 +0100

    Fix travis errors

--- src/ResultWriter/PostProcessor.cpp
+++ src/ResultWriter/PostProcessor.cpp
@@ -45,14 +45,14 @@ void seissol::writer::PostProcessor::integrateQuantities(const double i_timestep
 	const double * const i_dofs) {
 
 	real (*integrals) = i_layerData.var(m_integrals);
-	for (unsigned int i = 0; i < m_numberOfVariables; i++) {
+	for (int i = 0; i < m_numberOfVariables; i++) {
 		integrals[l_cell*m_numberOfVariables+i] += i_dofs[NUMBER_OF_ALIGNED_BASIS_FUNCTIONS*m_integerMap[i]]*i_timestep;
 	}
 }
 
 void seissol::writer::PostProcessor::setIntegrationMask(const int * const i_integrationMask) {
 	unsigned int nextId = 0;
-	for (unsigned int i = 0; i < 9; i++) {
+	for (int i = 0; i < 9; i++) {
 		m_integrationMask[i] = (bool)i_integrationMask[i];
 		if (m_integrationMask[i]) {
 			m_integerMap.push_back(nextId);
@@ -80,6 +80,6 @@ const double* seissol::writer::PostProcessor::getIntegrals(seissol::initializers
 	if (m_numberOfVariables == 0) {
 		return 0L;
 	} else {
-	return reinterpret_cast<const double*>(ltsTree->var(m_integrals));
+		return reinterpret_cast<const double*>(ltsTree->var(m_integrals));
 	}
 }
